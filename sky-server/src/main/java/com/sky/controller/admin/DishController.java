a33613cf78afd1c32d781ab0929fcb36b49213cb
aureliasusu <488140041@qq.com>
2025-02-26 17:42:53 -0800

    delete dish batch

--- sky-server/src/main/java/com/sky/controller/admin/DishController.java
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -1,15 +1,21 @@
 package com.sky.controller.admin;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.DishService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -24,6 +30,21 @@ public class DishController {
   public Result save(@RequestBody DishDTO dishDTO){
     log.info("新增菜品：{}", dishDTO);
     dishService.saveWithFlavor(dishDTO);
+    return Result.success();
+  }
+  @GetMapping("/page")
+  @ApiOperation("菜品分页查询")
+  public Result<PageResult> page(DishPageQueryDTO dishPageQueryDTO){
+    log.info("菜品分页查询：{}", dishPageQueryDTO);
+    PageResult pageResult = dishService.pageQuery(dishPageQueryDTO);
+    return Result.success(pageResult);
+  }
+
+  @DeleteMapping
+  @ApiOperation("菜品批量删除")
+  public Result delete(@RequestParam List<Long> ids){
+    log.info("菜品批量删除：{}", ids);
+    dishService.deleteBatch(ids);
     return Result.success();
   }
 
--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -2,6 +2,7 @@ package com.sky.mapper;
 
 import com.sky.entity.DishFlavor;
 import java.util.List;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -9,4 +10,7 @@ public interface DishFlavorMapper {
 
 
   void insertBatch(List<DishFlavor> flavors);
+
+  @Delete("delete from dish_flavor where dish_id = #{dishId}")
+  void deleteByDishId(Long dishId);
 }
