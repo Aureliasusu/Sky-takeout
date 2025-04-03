bf4d965fce9f4a817d0fbcda2406b10f52a3a039
aureliasusu <488140041@qq.com>
2025-04-02 20:02:33 -0700

    AddressBook

--- sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShoppingCartController.java
@@ -1,12 +1,16 @@
 package com.sky.controller.user;
 
 import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import com.sky.service.ShoppingCartService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,5 +30,20 @@ public class ShoppingCartController {
     shoppingCartService.addShoppingCart(shoppingCartDTO);
     return Result.success();
   }
+
+  @GetMapping("/list")
+  @ApiOperation("查看购物车")
+  public Result<List<ShoppingCart>> list(){
+    List<ShoppingCart> list = shoppingCartService.showShoppingCart();
+    return Result.success(list);
+  }
+
+  @DeleteMapping("/clean")
+  @ApiOperation("清空购物车")
+  public Result clean(){
+    shoppingCartService.cleanShoppingCart();
+    return Result.success();
+  }
+
 
 }
